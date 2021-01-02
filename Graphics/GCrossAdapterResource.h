709ae4a92d5bc61c2a8eb21162b99ccb3a723cfc
Mikhail Bogdanov <raeinforce@gmail.com>
2021-01-02 15:12:43 +0300

    Update cross adapter resource

--- Graphics/GCrossAdapterResource.h
+++ Graphics/GCrossAdapterResource.h
@@ -2,30 +2,37 @@
 #include <d3d12.h>
 #include <string>
 #include <memory>
+#include "GResource.h"
 
-namespace PEPEngine
+
+namespace PEPEngine::Graphics
 {
-	namespace Graphics
+
+	class GCrossAdapterResource
 	{
-		class GResource;
-		class GDevice;
+		std::shared_ptr<GResource> primeResource;
+		std::shared_ptr<GResource> sharedResource;
+		ComPtr<ID3D12Heap> crossAdapterResourceHeap[2];
 
-		class GCrossAdapterResource
-		{
-			std::shared_ptr<GResource> primeResource;
-			std::shared_ptr<GResource> sharedResource;
+		bool isInit = false;
 
-		public:
+	public:
 
-			GCrossAdapterResource(D3D12_RESOURCE_DESC& desc, const std::shared_ptr<GDevice>& primeDevice,
-			                      const std::shared_ptr<GDevice>
-			                      & sharedDevice, const std::wstring& name = L"",
-			                      D3D12_RESOURCE_STATES initialState = D3D12_RESOURCE_STATE_COMMON,
-			                      const D3D12_CLEAR_VALUE* clearValue = nullptr);
+		bool IsInit() const;
 
-			const GResource& GetPrimeResource() const;
+		GCrossAdapterResource() {  }
 
-			const GResource& GetSharedResource() const;
-		};
-	}
+		GCrossAdapterResource(D3D12_RESOURCE_DESC& desc, const std::shared_ptr<GDevice>& primeDevice,
+			const std::shared_ptr<GDevice>
+			& sharedDevice, const std::wstring& name = L"",
+			D3D12_RESOURCE_STATES initialState = D3D12_RESOURCE_STATE_COMMON,
+			const D3D12_CLEAR_VALUE* clearValue = nullptr);
+
+		const GResource& GetPrimeResource() const;
+
+		const GResource& GetSharedResource() const;
+		void Reset();
+
+		void Resize(UINT newWidth, UINT newHeight);
+	};
 }
